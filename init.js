3320807cd073747db8daeac784481807d0035cbf
shah-cloud-II <shahfahed@getfareye.com>
2021-09-24 17:34:21 +0530

    Mehul Jain | task has been done.

--- init.js
+++ init.js
@@ -1,5 +1,36 @@
 module.exports = {
   sum(a, b) {
-    
+    return a + b;
+  },
+  sortObjects(listsOfObjects, key) {
+    return listsOfObjects.sort((a, b) => {
+      if (a[key] > b[key]) {
+        return 1
+      }
+      if (a[key] < b[key]) {
+        return -1
+      }
+      return 0;
+    });
+  },
+  intelligentArray(listsOfObjects, operationType, key) {
+    if (operationType === 'sum') {
+      return listsOfObjects.reduce((prev, currentObj) => {
+        return prev += currentObj[key];
+      }, 0)
+    }
+    else if (operationType === 'avg') {
+      return listsOfObjects.reduce((prev, currentObj) => {
+        return prev += currentObj[key];
+      }, 0) / listsOfObjects.length;
+    }
+    else if (operationType === 'product') {
+      return listsOfObjects.reduce((prev, currentObj) => {
+        return prev *= currentObj[key];
+      }, 1)
+    }
+    else {
+      return "invaid Operation type.";
+    }
   }
 };
